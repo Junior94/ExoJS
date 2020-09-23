f26a8db5854f72dbaaa0ae1c6909e223f978c851
Junior <kuakuvidesire@gmail.com>
2020-09-23 12:56:07 +0200

    feat(feat): first commit exo js

---
+++ index.js
@@ -0,0 +1,48 @@
+// Exercice 1
+
+// My Digital School Forever
+
+//Exercice 2
+
+/* My Digital School
+   forever
+   every day
+*/
+
+// Exercice 3
+
+var string = '42';
+var number = 42;
+var float = 42.0;
+var bool = true | false;
+var array = [42];
+var object = {'42': 42};
+var date = new Date(4241, 12, 24, 42, 42);
+var a = null;
+var b;
+
+// Exercice 4
+
+var my42count = "quanrante-deux".length;
+
+// Exercice 5
+
+var value = 0;
+var check = value || 42;
+
+// console.log(check);
+
+// Exercice 6
+
+var myArray42 = ['q', 'u', 'a', 'r', 'a', 'n', 't', 'e', '-', 'd', 'e', 'u', 'x'];
+console.log(myArray42);
+
+// Exercice 7
+
+var myArray
+
+// Exercice 10
+
+var my42Type = typeof string;
+
+console.log(my42Type);

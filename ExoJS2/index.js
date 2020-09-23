12c984dde56164a5919f7f4e710966a0ef1231b1
Junior <kuakuvidesire@gmail.com>
2020-09-23 16:37:57 +0200

    feat(exojs2): other exercises of javascript

---
+++ ExoJS2/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* Exercice 1
+
+var age = prompt("Quel âge avez-vous ?");
+
+if(age < 18) {
+    alert("Désolé vous ne pouvez pas entrer")
+}else if ((age => 18) && (age <= 41)) { 
+    alert("Très bien vous pouvez entrer")
+}else {
+    alert("Vous êtes maintenant le patron !")
+}
+
+*/
+
+/* Exercice 2
+ 
+var rand = Math.floor(Math.random() * (30)) + 1;
+
+if ((rand >= 0) && (rand <= 10)) {
+    alert(rand + " : Cool !")
+} else if ((rand =>11) && (rand <=20)) {
+    alert(rand + " : Tepid !")
+} else {
+    alert(rand + " : Warm !")
+}
+
+*/
+
+// Exercice 3
